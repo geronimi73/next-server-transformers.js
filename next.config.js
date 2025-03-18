9c2331452c9553ba6e6645e57974431793acd19e
Tim Pietrusky <tim.pietrusky@runpod.io>
2025-03-18 13:04:42 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // (Optional) Export as a standalone site
+  // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
+  //   output: "standalone", // Feel free to modify/remove this option
+
+  // Indicate that these packages should not be bundled by webpack
+  serverExternalPackages: [
+    "onnxruntime-node",
+    "@huggingface/transformers",
+    "sharp",
+  ],
+};
+
+module.exports = nextConfig;
