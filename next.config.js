20254073b3dbf5e62154d24a0b5433eef9b98aca
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 06:30:31 +0200

    .

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   // see https://vercel.com/guides/troubleshooting-function-250mb-limit
   // and https://nextjs.org/docs/app/api-reference/config/next-config-js/output#caveats
-  outputFileTracingExcludes: {
-      '/': [
-        './node_modules/@huggingface/transformers/node_modules/onnxruntime-node/*'
-      ],
-  },
+  // outputFileTracingExcludes: {
+  //     '/': [
+  //       './node_modules/@huggingface/transformers/node_modules/onnxruntime-node/*'
+  //     ],
+  // },
 
   // Indicate that these packages should not be bundled by webpack
   // serverExternalPackages: [
