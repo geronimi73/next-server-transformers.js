d247ca7f7d3d4e9eb8b3450e16b9460da1d80043
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 07:09:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,15 +4,16 @@ const nextConfig = {
   // and https://nextjs.org/docs/app/api-reference/config/next-config-js/output#caveats
   outputFileTracingExcludes: {
       '/': [
-        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/*',
+        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/darwin/*',
+        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/win32/*',
+        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/linux/arm64/*',
         // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/linux/x64/onnxruntime_binding.node',
       ],
   },
-  outputFileTracingIncludes: {
-      '/': [
-        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/*',
-      ],
-  },
+  // outputFileTracingIncludes: {
+  //     '/': [
+  //     ],
+  // },
 
   // Indicate that these packages should not be bundled by webpack
   // serverExternalPackages: [
