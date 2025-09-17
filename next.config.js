b858a59ad845b6321f1137dabf09c93d3f4246e2
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 07:18:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
         'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/win32/**/*',
         'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/win32/**',
         'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/arm64/*',
+        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/*',
         // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/linux/x64/onnxruntime_binding.node',
       ],
   },
