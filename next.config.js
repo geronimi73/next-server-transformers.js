744ade85ba08c54565f0e6bc7860bc3c0a5181b1
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 08:55:06 +0200

    YES!

--- next.config.js
+++ next.config.js
@@ -1,39 +1,12 @@
-const path = require('path');
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // see https://vercel.com/guides/troubleshooting-function-250mb-limit
   // and https://nextjs.org/docs/app/api-reference/config/next-config-js/output#caveats
   outputFileTracingExcludes: {
       '/': [
-        // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime.so.1',
-        // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime.so.1.20.1',
-        // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime_providers_shared.so',
-        // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime.so*',
         'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/!(libonnxruntime.so.1|onnxruntime_binding.node)',
       ],
   },
-  outputFileTracingIncludes: {
-      '/': [
-        // 'node_modules/@huggingface/transformers/**/libonnxruntime.so.1',
-      ],
-  },
-
-  // Indicate that these packages should not be bundled by webpack
-  // serverExternalPackages: [
-  //   "onnxruntime-node",
-  //   "@huggingface/transformers",
-  //   // "sharp",
-  // ],
-  // webpack: (config) => {
-  //     config.resolve.alias = {
-  //       ...config.resolve.alias,
-  //       "onnxruntime-node": path.join(__dirname, 'node_modules/onnxruntime-node/dist/bindkjhings.js'),
-  //     }
-
-  //     return config;
-  // },
 };
 
 module.exports = nextConfig;
