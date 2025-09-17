5aece5c0bdcfff63f02a15a12cf76a707a3426f0
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-09-17 08:31:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
         // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime_providers_shared.so',
         // 'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/libonnxruntime.so*',
         'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/!(libonnxruntime.so.1)',
+        'node_modules/@huggingface/transformers/node_modules/onnxruntime-node/bin/napi-v3/linux/x64/!(onnxruntime_binding.node)',
       ],
   },
   outputFileTracingIncludes: {
